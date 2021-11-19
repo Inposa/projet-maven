4d7704597137d984331eca3af6db77d0bd14cd1f
Inposa <donhippo04@gmail.com>
2021-11-19 15:30:30 +0100

    modifs on files

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,12 +2,11 @@ package dev;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello tout le monde !");
+        System.out.println("Hello World!");
+
     }
 }
